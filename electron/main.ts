634765f87c3636fcf4cd0bcb9d83108a56fbe8d1
zhangxuyang <240283142@qq.com>
2023-11-05 10:09:50 +0800

    chore: 修改 electron 扩展目录结构

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,5 @@
-import {app, session, BrowserWindow} from 'electron'
-import path from 'node:path'
+import { app, session, BrowserWindow } from "electron";
+import path from "node:path";
 
 // The built directory structure
 //
@@ -10,70 +10,72 @@ import path from 'node:path'
 // │ │ ├── main.js
 // │ │ └── preload.js
 // │
-process.env.DIST = path.join(__dirname, '../dist')
-process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+process.env.DIST = path.join(__dirname, "../dist");
+process.env.VITE_PUBLIC = app.isPackaged
+  ? process.env.DIST
+  : path.join(process.env.DIST, "../public");
 
-
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
-
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 
 // 用于添加Chromium插件
 async function setupDevTools() {
-    // // 安装 vue 开发者工具
-    // const {
-    //     default: installExtension,
-    //     VUEJS3_DEVTOOLS
-    // } = await import("electron-devtools-installer");
-    // return installExtension([
-    //     VUEJS3_DEVTOOLS
-    // ]);
-    return await session.defaultSession.loadExtension(path.resolve("electron/vue3_devtools_6.5.1_0"))
+  // // 安装 vue 开发者工具
+  // const {
+  //     default: installExtension,
+  //     VUEJS3_DEVTOOLS
+  // } = await import("electron-extensions-installer");
+  // return installExtension([
+  //     VUEJS3_DEVTOOLS
+  // ]);
+  return await session.defaultSession.loadExtension(
+    path.resolve("electron/extensions/vue3_devtools_6.5.1_0"),
+  );
 }
 
 async function createWindow() {
-    win = new BrowserWindow({
-        icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        },
-    })
+  win = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-    // Test active push message to Renderer-process.
-    win.webContents.on('did-finish-load', () => {
-        win?.webContents.send('main-process-message', (new Date).toLocaleString())
-    })
+  // Test active push message to Renderer-process.
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
 
-    if (VITE_DEV_SERVER_URL) {
-        win.loadURL(VITE_DEV_SERVER_URL)
-    } else {
-        // win.loadFile('dist/index.html')
-        win.loadFile(path.join(process.env.DIST, 'index.html'))
-    }
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    // win.loadFile('dist/index.html')
+    win.loadFile(path.join(process.env.DIST, "index.html"));
+  }
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-        win = null
-    }
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
+  }
+});
 
-app.on('activate', async () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        await createWindow()
-    }
-})
+app.on("activate", async () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    await createWindow();
+  }
+});
 
 app.whenReady().then(async () => {
-    await createWindow()
-    if (VITE_DEV_SERVER_URL) {
-        await setupDevTools()
-    }
-})
+  await createWindow();
+  if (VITE_DEV_SERVER_URL) {
+    await setupDevTools();
+  }
+});
